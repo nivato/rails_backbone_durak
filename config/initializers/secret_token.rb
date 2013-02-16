5f7e70bfe239acd8514981ee745b4a7e389ed32e
nivato <nazarivato@gmail.com>
2013-02-16 22:57:27 +0200

    Created rails application structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBackboneDurak::Application.config.secret_token = '5707b567bcae7e394dd2fa89aee1c6e4e73edbe51eb09f72f41d1eee7fc683b0ea5ea2241f82bae065691f53a450698969d880d250a3c62960fc9cb5c1a1fa5f'
