6417393825b3999d051442af3b38de050a733998
nivato <nazarivato@gmail.com>
2013-02-17 15:41:30 +0200

    Added setup_game_session method to setup game and assign all cards to the Deck;

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,16 +1,34 @@
 class GameController < ApplicationController
   
+  before_filter :setup_game_session
+  
   def index
+    pupulate_db
+    redirect_to :controller => "cards", :action => "index"
+  end
+  
+  private
+  
+  def setup_game_session
+    unless session[:game_session]
+      game_session = Digest::SHA2.hexdigest("#{Time.now}")
+      session[:game_session] = game_session
+      game = Game.create(:game_session => game_session)
+      deck = Cardholder.deck.first
+      Card.all.each do |card|
+        GameLog.create(:game_id => game.id, :cardholder_id => deck.id, :card_id => card.id)
+      end
+    end
+  end
+  
+  def pupulate_db
     if Card.all == []
       pupulate_db_with_cards
     end
     if Cardholder.all == []
       populate_db_with_cardholders
     end
-    redirect_to :controller => "cards", :action => "index"
   end
-  
-  private
   
   def pupulate_db_with_cards
     ranks = ["6", "7", "8", "9", "10", "J", "Q", "K", "A"]
--- app/models/cardholder.rb
+++ app/models/cardholder.rb
@@ -4,4 +4,9 @@ class Cardholder < ActiveRecord::Base
   has_many :games, :through => :game_logs
   has_many :cards, :through => :game_logs
   
+  scope :player, where(:ch_type => "Player")
+  scope :deck, where(:ch_type => "Deck")
+  scope :table, where(:ch_type => "Table")
+  scope :computer, where(:ch_type => "Computer")
+  scope :pile, where(:ch_type => "Pile")
 end
