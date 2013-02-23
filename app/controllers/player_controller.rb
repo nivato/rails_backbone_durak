6a2cde43b044708f9503c5151283b729134ebcae
unknown <nazarivato@gmail.com>
2013-02-23 14:11:37 +0200

    Created Computer.beat_card(game, card); Moved all computer-related code from player_controller to computer;

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -13,24 +13,7 @@ class PlayerController < ApplicationController
     card = Card.find(params[:id])
     game = Game.for_session(session[:game_session]).first
     Player.play_card(game, card)
-    
-    table = Cardholder.table.first
-    # make computer to choose card to beat with
-    computer = Cardholder.computer.first
-    computer_cards = computer.cards.where("game_logs.game_id = #{game.id}").order("game_logs.position ASC")
-    attacker_card_to_beat = card
-    card_to_beat_with = choose_card_to_beat_with(attacker_card_to_beat, computer_cards, Deck.get_trump(game))
-    if card_to_beat_with != nil
-      c_game_log = computer.game_logs.where(:game_id => game.id, :card_id => card_to_beat_with.id).first
-      c_game_log.update_attributes(:cardholder_id => table.id, :played_by => computer.id, :position => Defender.get_cards_max_position(game) + 1)
-      # mark computer with defender_state = "won" in case player has no playable cards
-      if Player.get_playable_cards(game) == []
-        game.update_attribute("defender_state", "won")
-      end
-    else
-      game.update_attribute("defender_state", "defeated")
-    end
-    
+    Computer.beat_card(game, card)
     respond_with []
   end
 
@@ -61,63 +44,6 @@ class PlayerController < ApplicationController
   # PUT /decks/1.json
   def update
     respond_with []
-  end
-  
-  private
-  
-  def choose_card_to_beat_with(attacker_card_to_beat, computer_cards, trump)
-    card_to_beat_with = nil
-    if attacker_card_to_beat.suit_char == trump.suit_char
-      card_to_beat_with = choose_higher_card_of_same_suit(attacker_card_to_beat, computer_cards)
-    else
-      card_to_beat_with = choose_higher_card_or_trump(attacker_card_to_beat, computer_cards, trump)
-    end
-    logger.info("CARD TO BEAT WITH #{card_to_beat_with.inspect}")
-    return card_to_beat_with
-  end
-  
-  def choose_higher_card_or_trump(attacker_card_to_beat, computer_cards, trump)
-    card_to_beat_with = choose_higher_card_of_same_suit(attacker_card_to_beat, computer_cards)
-    unless card_to_beat_with
-      cards_of_trump_suit = computer_cards.collect do |card|
-        card if card.suit_char == trump.suit_char
-      end
-      cards_of_trump_suit.compact!
-      unless cards_of_trump_suit == []
-        ranks = cards_of_trump_suit.collect do |card|
-          card.rank_number
-        end
-        min_rank = ranks.to_a.min
-        cards_of_trump_suit.each do |card|
-          card_to_beat_with = card if card.rank_number == min_rank
-        end
-      end
-    end
-    return card_to_beat_with
-  end
-    
-  def choose_higher_card_of_same_suit(attacker_card_to_beat, computer_cards)
-    card_to_beat_with = nil
-    cards_of_same_suit = computer_cards.collect do |card|
-        card if card.suit_char == attacker_card_to_beat.suit_char
-    end
-    cards_of_same_suit.compact!
-    unless cards_of_same_suit == []
-      cards_of_higher_rank = cards_of_same_suit.collect do |card|
-        card if card.rank_number > attacker_card_to_beat.rank_number
-      end
-      cards_of_higher_rank.compact!
-      unless cards_of_higher_rank == []
-        ranks = cards_of_higher_rank.collect do |card|
-          card.rank_number
-        end
-        min_rank = ranks.to_a.min
-        cards_of_higher_rank.each do |card|
-          card_to_beat_with = card if card.rank_number == min_rank
-        end
-      end
-    end
-    return card_to_beat_with
   end
 
 end
--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,6 +1,80 @@
 class Computer
+  
   def self.get_cards(game)
     computer = Cardholder.computer.first
     return computer.cards.select("cards.*, game_logs.position").where("game_logs.game_id = #{game.id}").order("game_logs.position ASC")
   end
+  
+  def self.beat_card(game, attackers_card)
+    computer_cards = get_cards(game)
+    defenders_card = choose_defending_card(attackers_card, computer_cards, Deck.get_trump(game))
+    if defenders_card != nil
+      computer = Cardholder.computer.first
+      game_log = computer.game_logs.where(:game_id => game.id, :card_id => defenders_card.id).first
+      table = Cardholder.table.first
+      game_log.update_attributes(:cardholder_id => table.id, :played_by => computer.id, :position => Defender.get_cards_max_position(game) + 1)
+      if Player.get_playable_cards(game) == []
+        game.update_attribute("defender_state", "won")
+      end
+    else
+      game.update_attribute("defender_state", "defeated")
+    end
+  end
+  
+  private
+  
+  def self.choose_defending_card(attackers_card, computer_cards, trump)
+    defenders_card = nil
+    if attackers_card.suit_char == trump.suit_char
+      defenders_card = choose_higher_card_of_same_suit(attackers_card, computer_cards)
+    else
+      defenders_card = choose_higher_card_or_trump(attackers_card, computer_cards, trump)
+    end
+    return defenders_card
+  end
+  
+  def self.choose_higher_card_or_trump(attackers_card, computer_cards, trump)
+    defenders_card = choose_higher_card_of_same_suit(attackers_card, computer_cards)
+    unless defenders_card
+      cards_of_trump_suit = computer_cards.collect do |card|
+        card if card.suit_char == trump.suit_char
+      end
+      cards_of_trump_suit.compact!
+      unless cards_of_trump_suit == []
+        ranks = cards_of_trump_suit.collect do |card|
+          card.rank_number
+        end
+        min_rank = ranks.to_a.min
+        cards_of_trump_suit.each do |card|
+          defenders_card = card if card.rank_number == min_rank
+        end
+      end
+    end
+    return defenders_card
+  end
+    
+  def self.choose_higher_card_of_same_suit(attackers_card, computer_cards)
+    defenders_card = nil
+    cards_of_same_suit = computer_cards.collect do |card|
+        card if card.suit_char == attackers_card.suit_char
+    end
+    cards_of_same_suit.compact!
+    unless cards_of_same_suit == []
+      cards_of_higher_rank = cards_of_same_suit.collect do |card|
+        card if card.rank_number > attackers_card.rank_number
+      end
+      cards_of_higher_rank.compact!
+      unless cards_of_higher_rank == []
+        ranks = cards_of_higher_rank.collect do |card|
+          card.rank_number
+        end
+        min_rank = ranks.to_a.min
+        cards_of_higher_rank.each do |card|
+          defenders_card = card if card.rank_number == min_rank
+        end
+      end
+    end
+    return defenders_card
+  end
+  
 end
